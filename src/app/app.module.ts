4fed86bd6d162643c11bc1e98e69a723abf3467f
Shantonav Sen <shantonav.sen@billinghouse.nl>
2020-09-13 22:58:15 +0200

    [SOLUTION-FIX] : added schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ] to NgModules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
@@ -19,7 +19,8 @@ import { FieldErrorDisplayComponent } from './field-error-display/field-error-di
     AppComponent,
     FieldErrorDisplayComponent
   ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  schemas: [ CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA ]
 })
 
 
