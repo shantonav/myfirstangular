8f2f836ff445b8b4c22bddcc5cb3c3520cd7999a
Shantonav Sen <shantonav.sen@billinghouse.nl>
2020-09-13 03:10:37 +0200

    [partr]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Route } from '@angular/router';
+import { AppComponent } from './app.component';
 import { HttpModule } from '@angular/http';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 @NgModule({
   /* set respective calls to components, services, etc here */
   providers: [],
-  bootstrap: []
+  imports: [
+    // other imports ...
+    BrowserModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot([
+      { path: '', component: AppComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 
+
 export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
